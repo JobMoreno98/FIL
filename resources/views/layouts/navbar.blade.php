1bcb6a0942021bd08ff3702647a3f32390921b31
Job Moreno <jobmoreno98@gmail.com>
2022-10-28 12:50:13 -0500

    Navbar corregida

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-md bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="{{route('inicio')}}">FIL</a>
+        <a class="navbar-brand" href="{{ route('inicio') }}">FIL</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
             aria-controls="offcanvasNavbar">
             <span class="navbar-toggler-icon"></span>
@@ -13,39 +13,39 @@
             <div class="offcanvas-body">
                 <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="{{route('inicio')}}">Inicio</a>
+                        <a class="nav-link active" aria-current="page" href="{{ route('inicio') }}">Inicio</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{route('agenda.index')}}">Administración</a>
+                        <a class="nav-link" href="{{ route('agenda.index') }}">Administración</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{route('miagenda')}}">Mi Agenda</a>
+                        <a class="nav-link" href="{{ route('miagenda') }}">Mi Agenda</a>
                     </li>
-                </ul>
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ms-auto">
                     <!-- Authentication Links -->
                     @guest
-                        <li><a class="p-2" href="{{ route('login') }}">{{ __('Acceder') }}</a></li>
-                        <li><a class="p-2" href="{{ route('register') }}">{{ __('Registrarse') }}</a></li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Acceder') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Registrarse') }}</a>
+                        </li>
                     @else
                         <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
                                 {{ Auth::user()->name }}
                             </a>
-
-                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                            <ul class="dropdown-menu">
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                     onclick="event.preventDefault();
-                                                                         document.getElementById('logout-form').submit();">
+                                                                 document.getElementById('logout-form').submit();">
                                     Cerrar Sesión
                                 </a>
 
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                     @csrf
                                 </form>
-                            </div>
+                            </ul>
                         </li>
                     @endguest
                 </ul>
